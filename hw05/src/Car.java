97d6333174772c06563ea81525b66253732ee0ea
Maxim Timinskiy <timinmax@gmail.com>
2020-04-10 11:33:17 +0300

    Final version with winner identification

--- hw05/src/Car.java
+++ hw05/src/Car.java
@@ -1,3 +1,4 @@
+import java.util.concurrent.CountDownLatch;
 
 public class Car implements Runnable {
     private static int CARS_COUNT;
@@ -7,15 +8,19 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    private CountDownLatch startFlag,finishFlag;
+
     public String getName() {
         return name;
     }
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, CountDownLatch startFlag,CountDownLatch finishFlag, int speed) {
         this.race = race;
         this.speed = speed;
+        this.startFlag = startFlag;
+        this.finishFlag = finishFlag;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
@@ -28,8 +33,20 @@ public class Car implements Runnable {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        startFlag.countDown();
+        try {
+            startFlag.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        if (finishFlag.getCount() == CARS_COUNT){
+            System.out.println(getName() + " - WIN");
+            //на одном из прогонов программы я получил 2 WIN.
+            //Больше это не удалось воспроизвести.
+        }
+        finishFlag.countDown();
     }
 }
--- hw05/src/MainClass.java
+++ hw05/src/MainClass.java
@@ -1,16 +1,30 @@
+import java.util.concurrent.CountDownLatch;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Race race = new Race(new Road(60), new Tunnel(Math.round(CARS_COUNT/2)), new Road(40));
+        CountDownLatch startFlag = new CountDownLatch(CARS_COUNT);
+        CountDownLatch finishFlag = new CountDownLatch(CARS_COUNT);
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, startFlag, finishFlag ,20 + (int) (Math.random() * 10));
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
+        try {
+            startFlag.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        try {
+            finishFlag.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
